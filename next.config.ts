05a42de1217a1100d04346855e6c0d80e77b86cd
zavocc <buffer.0verfl0w1030@gmail.com>
2026-01-31 06:35:46 +0800

    tess 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Exclude large SWC binaries from the Netlify server function bundle,
+  // but keep @swc/helpers which Next needs at runtime.
+  outputFileTracingExcludes: {
+    "*": [
+      "**/node_modules/@next/swc-*/**",
+      "**/node_modules/@swc/core-*/**",
+      "**/node_modules/@swc/core/**",
+    ],
+  },
+  outputFileTracingIncludes: {
+    "*": ["**/node_modules/@swc/helpers/**"],
+  },
 };
 
 export default nextConfig;
