a67898a0d0df1e7a5315e078ba99240ea9641c76
zavocc <buffer.0verfl0w1030@gmail.com>
2026-01-31 06:37:21 +0800

    Revert "tess 2"
    This reverts commit 05a42de1217a1100d04346855e6c0d80e77b86cd.

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Exclude large SWC binaries from the Netlify server function bundle,
-  // but keep @swc/helpers which Next needs at runtime.
-  outputFileTracingExcludes: {
-    "*": [
-      "**/node_modules/@next/swc-*/**",
-      "**/node_modules/@swc/core-*/**",
-      "**/node_modules/@swc/core/**",
-    ],
-  },
-  outputFileTracingIncludes: {
-    "*": ["**/node_modules/@swc/helpers/**"],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
